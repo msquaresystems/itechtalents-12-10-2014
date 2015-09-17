979370e1eb79cffa6d2d60c05eade49e9226137f
Dariem Pérez Herrera <dariemp@gmail.com>
2015-09-17 14:54:16 -0500

    Fix: create site row if it does not exists.

--- registration/migrations/9999_set_site_name.py
+++ registration/migrations/9999_set_site_name.py
@@ -7,10 +7,15 @@ from django.db import models, migrations
 def set_site_name(apps, schema_editor):
     from django.conf import settings
     from django.contrib.sites.models import Site
-    site = Site.objects.get(id=settings.SITE_ID)
-    site.domain = settings.SITE_NAME
-    site.name = settings.SITE_NAME
-    site.save()
+    try:
+        site = Site.objects.get(id=settings.SITE_ID)
+        site.domain = settings.SITE_NAME
+        site.name = settings.SITE_NAME
+        site.save()
+    except Site.DoesNotExist:
+        Site.objects.create(id=settings.SITE_ID,
+                            domain=settings.SITE_NAME,
+                            name=settings.SITE_NAME)
 
 
 class Migration(migrations.Migration):
